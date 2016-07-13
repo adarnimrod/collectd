54f1f8786bf34343ab1c713f3e312ba2eed4cf0a
Adar Nimrod <nimrod@shore.co.il>
2016-07-13 21:03:56 +0300

    - Added email alias to root. - Added some more tests.

--- tests/test_collectd.py
+++ tests/test_collectd.py
@@ -1,2 +1,23 @@
-def test_collectd(Service):
-    assert Service('collectd').is_running
+def test_collectd_service(Service):
+    service = Service('collectd')
+    assert service.is_running
+    try:
+        assert service.is_enabled
+    except NotImplementedError:
+        pass
+
+
+def test_collectd_config(File, Command):
+    assert Command('collectd -t').rc == 0
+    assert File('/etc/collectd.conf').is_file or File(
+        '/etc/collectd/collectd.conf').is_file
+    assert File('/etc/collectd.conf.d').is_directory or File(
+        '/etc/collectd/collectd.conf.d').is_directory
+
+
+def test_collectd_alias(File, Ansible):
+    ansible_os_family = Ansible('setup')['ansible_facts']['ansible_os_family']
+    if ansible_os_family == 'Debian':
+        assert File('/etc/aliases').contains('collectd: root')
+    elif ansible_os_family == 'OpenBSD':
+        assert File('/etc/mail/aliases').contains('_collectd: root')
